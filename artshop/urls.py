a545c3aed2cab0e6d863b9aef36304cdce73f482
Francisco Sabino <Francisco Sabino>
2021-11-11 08:54:43 +0100

    created products app

--- artshop/urls.py
+++ artshop/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", include("home.urls")),
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path("products/", include("products.urls")),
 ]
