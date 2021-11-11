109257e6ff4f1f3bdf038ede24cbf94f935503f0
Francisco Sabino <Francisco Sabino>
2021-11-11 09:26:14 +0100

    added a verbose name to category field

--- products/models.py
+++ products/models.py
@@ -2,8 +2,13 @@ from django.db import models
 
 
 class Category(models.Model):
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
 
     def __str__(self):
         return self.name
