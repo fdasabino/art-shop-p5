d8fba0c0bf5a5ce01939178ce818d96e648cdddb
Francisco Sabino <Francisco Sabino>
2021-11-11 09:19:32 +0100

    created product models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
+from .models import Category, Product
+
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        "sku",
+        "name",
+        "category",
+        "price",
+        "rating",
+    )
+    ordering = ("sku",)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "friendly_name",
+        "name",
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
