9db3787c329ece816627b07a368c3e8453208a1d
Francisco Sabino <Francisco Sabino>
2021-11-13 02:06:05 +0100

    profile basic functionality

--- checkout/views.py
+++ checkout/views.py
@@ -1,14 +1,14 @@
 import json
 
 import stripe
+from cart.context_processor import cart_contents
 from django.conf import settings
 from django.contrib import messages
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render, reverse
+from django.shortcuts import HttpResponse, get_object_or_404, redirect, render, reverse
 from django.views.decorators.http import require_POST
-
-from cart.context_processor import cart_contents
 from products.models import Product
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -29,18 +29,11 @@ def cache_checkout_data(request):
         )
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(
-            request,
-            (
-                "Sorry, your payment cannot be "
-                "processed right now. Please try "
-                "again later."
-            ),
-        )
+        messages.error(request, ("Sorry, your payment cannot be " "processed right now. Please try " "again later."))
         return HttpResponse(content=e, status=400)
 
 
-def checkout(request):  # sourcery no-metrics
+def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
@@ -58,6 +51,7 @@ def checkout(request):  # sourcery no-metrics
             "street_address2": request.POST["street_address2"],
             "county": request.POST["county"],
         }
+
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save(commit=False)
@@ -88,21 +82,19 @@ def checkout(request):  # sourcery no-metrics
                     messages.error(
                         request,
                         (
-                            "One of the products in your cart wasn't found in our database. "
+                            "One of the products in your cart wasn't "
+                            "found in our database. "
                             "Please call us for assistance!"
                         ),
                     )
                     order.delete()
                     return redirect(reverse("view_cart"))
 
+            # Save the info to the user's profile if all is well
             request.session["save_info"] = "save-info" in request.POST
             return redirect(reverse("checkout_success", args=[order.order_number]))
         else:
-            messages.error(
-                request,
-                "There was an error with your form. \
-                Please double check your information.",
-            )
+            messages.error(request, ("There was an error with your form. " "Please double check your information."))
     else:
         cart = request.session.get("cart", {})
         if not cart:
@@ -118,13 +110,32 @@ def checkout(request):  # sourcery no-metrics
             currency=settings.STRIPE_CURRENCY,
         )
 
-        order_form = OrderForm()
+        # Attempt to prefill the form with any info
+        # the user maintains in their profile
+        if request.user.is_authenticated:
+            try:
+                profile = UserProfile.objects.get(user=request.user)
+                order_form = OrderForm(
+                    initial={
+                        "full_name": profile.user.get_full_name(),
+                        "email": profile.user.email,
+                        "phone_number": profile.default_phone_number,
+                        "country": profile.default_country,
+                        "postcode": profile.default_postcode,
+                        "town_or_city": profile.default_town_or_city,
+                        "street_address1": profile.default_street_address1,
+                        "street_address2": profile.default_street_address2,
+                        "county": profile.default_county,
+                    }
+                )
+            except UserProfile.DoesNotExist:
+                order_form = OrderForm()
+        else:
+            order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(
-            request,
-            "Stripe public key is missing. \
-            Did you forget to set it in your environment?",
+            request, ("Stripe public key is missing. " "Did you forget to set it in " "your environment?")
         )
 
     template = "checkout/checkout.html"
@@ -143,6 +154,28 @@ def checkout_success(request, order_number):
     """
     save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                "default_phone_number": order.phone_number,
+                "default_country": order.country,
+                "default_postcode": order.postcode,
+                "default_town_or_city": order.town_or_city,
+                "default_street_address1": order.street_address1,
+                "default_street_address2": order.street_address2,
+                "default_county": order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
     messages.success(
         request,
         f"Order successfully processed! \
