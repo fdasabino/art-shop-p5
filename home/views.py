0f1bdbc9c18fdc8b73c1258499ebe79001b025d5
Francisco Sabino <fdasabino@gmail.com>
2021-11-16 13:10:29 +0100

    updated a few elements across the page templates

--- home/views.py
+++ home/views.py
@@ -8,3 +8,17 @@ def index(request):
     Render the homepage view
     """
     return render(request, "home/index.html")
+
+
+def about(request):
+    """
+    Render the about view
+    """
+    return render(request, "home/about.html")
+
+
+def contact(request):
+    """
+    Render the contact page view
+    """
+    return render(request, "home/contact.html")
