2ab749b0241362aa21997fbbaa9cebb0110fc061
Francisco Sabino <fdasabino@gmail.com>
2021-11-18 06:42:50 +0100

    added contact page template and set up views and urls

--- artshop/settings.py
+++ artshop/settings.py
@@ -108,7 +108,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
@@ -197,13 +196,12 @@ STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET", "")
 
-# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-# DEFAULT_FROM_EMAIL = "danielasartshop@gmail.com"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # console
+DEFAULT_FROM_EMAIL = "danielasartshop@gmail.com"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  # smtp
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
-DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASS = os.environ.get("EMAIL_HOST_PASS")
--- home/views.py
+++ home/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import ContactForm
 
 
 def index(request):
@@ -18,7 +22,25 @@ def about(request):
 
 
 def contact(request):
-    """
-    Render the contact page view
-    """
-    return render(request, "home/contact.html")
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = "Website Inquiry"
+            body = {
+                "full_name": form.cleaned_data["full_name"],
+                "email": form.cleaned_data["email_address"],
+                "message": form.cleaned_data["message"],
+            }
+            message = "\n".join(body.values())
+
+            try:
+                send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list=[""])
+                messages.success(
+                    request, "Your message has been successfully sent. We will be in touch as soon as possible"
+                )
+            except BadHeaderError:
+                return HttpResponse("Invalid header found.")
+            return redirect("home")
+
+    form = ContactForm()
+    return render(request, "home/contact.html", {"form": form})
