5201b620003af24701eeb6d9eebf89e4311dd53e
Francisco Sabino <Francisco Sabino>
2021-11-19 11:46:20 +0100

    test deployment

--- artshop/settings.py
+++ artshop/settings.py
@@ -28,7 +28,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = False
 
 ALLOWED_HOSTS = ["danielasartshop.herokuapp.com", "127.0.0.1", "localhost"]
 
@@ -196,12 +197,12 @@ STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET", "")
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # console
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"  # console
 DEFAULT_FROM_EMAIL = "fdasabino@gmail.com"
 
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  # smtp
-# EMAIL_USE_TLS = True
-# EMAIL_PORT = 587
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-# EMAIL_HOST_PASS = os.environ.get("EMAIL_HOST_PASS")
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"  # smtp
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASS = os.environ.get("EMAIL_HOST_PASS")
